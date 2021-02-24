891c8c6eeddebe1ab48d675a9bc40bfcd139d7df
Poli_tn62@student.itstep.kz <Poli_tn62@student.itstep.kz>
2021-02-24 21:42:16 +0600

    Добавьте файлы проекта.

---
+++ SQLTransactionHomeWork/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace SQLTransactionHomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection connection = new SqlConnection();
+            connection.ConnectionString = "Server=DESKTOP-619RM10;Database=ConectionLesson;Trusted_Connection=True;";
+            connection.Open();
+
+            string sqlScript = "CREATE TABLE MyGroup (id int, Name nvarchar );";
+            using (var transaction = connection.BeginTransaction())
+            using (SqlCommand command = new SqlCommand(sqlScript, connection))
+            {
+                try
+                {
+                    command.Transaction = transaction;
+                    command.ExecuteNonQuery();
+                    transaction.Commit();
+                }
+                catch (SqlException exception)
+                {
+                    Console.WriteLine(exception.Message);
+                    transaction.Rollback();
+                }
+            }
+
+
+        }
+    }
+}
